af6f318c2711fdf7aae330a424e5e6f0f65d50ce
Faizur-brainstation23 <faizur.rahman@brainstation-23.com>
2025-07-11 18:27:22 +0600

    Delete Test.java

--- Test.java
+++
@@ -1,8 +0,0 @@
-package inheritance;
-
-public class Test {
-    public static void main(String[] args) {
-        Teacher t1 = new Teacher("Faizur", 30, "Internship");
-        t1.displayInformation();
-    }
-}
